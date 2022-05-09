03e0554db9596e3546a09049a80e395af4b92018
Christof <bauer.christof@gmail.com>
2022-05-08 23:29:30 -0500

    Add header to admin

--- pages/admin.js
+++ pages/admin.js
@@ -1,13 +1,22 @@
+import { supabase } from '../lib/supabase'
+import Head from 'next/head'
 import Clients from '../components/admin/Clients'
 import Dogs from '../components/admin/Dogs'
-import { supabase } from '../lib/supabase'
 
 const Index = ({ clients, dogs }) => {
   return (
-    <div className='py-8 dark:text-brand'>
-      <Clients clients={clients} />
-      <Dogs dogs={dogs} />
-    </div>
+
+    <>
+      <Head>
+        <title>Admin | Dog's Paradise</title>
+        <meta name='description' content="Admin | Dog's Paradise" />
+      </Head>
+
+      <div className='py-8 dark:text-brand'>
+        <Clients clients={clients} />
+        <Dogs dogs={dogs} />
+      </div>
+    </>
   )
 }
 
