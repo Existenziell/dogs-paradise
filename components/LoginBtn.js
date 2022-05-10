2b82c4d04797004733b86753f040f606c9557ee3
Christof <bauer.christof@gmail.com>
2022-05-10 01:31:06 -0500

    Enable PWA

--- components/LoginBtn.js
+++ components/LoginBtn.js
@@ -4,7 +4,7 @@ export const LoginBtn = () => {
   const user = supabase.auth.user()
 
   return (
-    <a href={user ? `/profile` : `/profile`} className="mr-4">
+    <a href={user ? `/profile` : `/profile`} className="mr-4" name='profile' aria-label='Go to Profile'>
       <svg xmlns="http://www.w3.org/2000/svg" className="h-12 w-12 text-brand hover:text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
         <path strokeLinecap="round" strokeLinejoin="round" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
       </svg>
