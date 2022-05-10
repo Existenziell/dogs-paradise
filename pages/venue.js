943c0098998bd7629cf23245e6ebc1e58896cece
Christof <bauer.christof@gmail.com>
2022-05-10 02:27:15 -0500

    Update venue.js

--- pages/venue.js
+++ pages/venue.js
@@ -12,14 +12,15 @@ const Venue = () => {
       </Head>
 
       <div className='py-24'>
-        <h1 className='text-4xl mb-8'>Venue</h1>
+        <h1 className='text-6xl mb-8'>Venue</h1>
+        <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         <div className='grid grid-cols-2 gap-8 items-center p-12'>
-          <img src='/img/venue/1.jpg' alt='Venue1' className='' />
-          <img src='/img/venue/2.jpg' alt='Venue2' className='' />
-          <img src='/img/venue/3.jpg' alt='Venue3' className='' />
-          <img src='/img/venue/4.jpg' alt='Venue4' className='' />
-          <img src='/img/venue/5.jpg' alt='Venue5' className='' />
-          <img src='/img/venue/6.jpg' alt='Venue6' className='' />
+          <img src='/img/venue/1.jpg' alt='Venue1' className='shadow-lg rounded' />
+          <img src='/img/venue/2.jpg' alt='Venue2' className='shadow-lg rounded' />
+          <img src='/img/venue/3.jpg' alt='Venue3' className='shadow-lg rounded' />
+          <img src='/img/venue/4.jpg' alt='Venue4' className='shadow-lg rounded' />
+          <img src='/img/venue/5.jpg' alt='Venue5' className='shadow-lg rounded' />
+          <img src='/img/venue/6.jpg' alt='Venue6' className='shadow-lg rounded' />
         </div>
       </div>
     </>
