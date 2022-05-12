b5c3294681f339b217ea7143f1e5f6ca6f5c190a
Christof <bauer.christof@gmail.com>
2022-05-11 21:20:21 -0500

    Control Panel v2 :cherries:

--- components/Nav.js
+++ components/Nav.js
@@ -53,11 +53,11 @@ const Nav = () => {
             {/* Mobile Hamburger Button */}
             <button className='mobile-menu-button outline-none mt-4 ml-4' onClick={() => setIsOpen(!isOpen)} aria-label='Open Mobile Navigation'>
                 {!isOpen ?
-                    <svg xmlns='http://www.w3.org/2000/svg' className='h-14 w-14 text-brand hover:text-slate-400' fill='none' strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' viewBox='0 0 24 24' stroke='currentColor'>
+                    <svg xmlns='http://www.w3.org/2000/svg' className='h-14 w-14 text-brand hover:text-slate-400 hover:scale-105' fill='none' strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' viewBox='0 0 24 24' stroke='currentColor'>
                         <path d='M4 6h16M4 12h16M4 18h16'></path>
                     </svg>
                     :
-                    <svg xmlns='http://www.w3.org/2000/svg' className='absolute top-4 left-4 h-14 w-14 z-20 text-gray-500 hover:text-brand dark:text-gray-300' fill='none' viewBox='0 0 24 24' stroke='currentColor'>
+                    <svg xmlns='http://www.w3.org/2000/svg' className='absolute top-4 left-4 h-14 w-14 z-20 text-gray-500 hover:text-brand dark:text-gray-300 hover:scale-105' fill='none' viewBox='0 0 24 24' stroke='currentColor'>
                         <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M6 18L18 6M6 6l12 12' />
                     </svg>
                 }
--- components/_Layout.js
+++ components/_Layout.js
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { useRouter } from 'next/router'
 import LoginBtn from './LoginBtn'
 import LogoutBtn from './LogoutBtn'
@@ -11,10 +12,19 @@ import StickyHeader from './StickyHeader'
 const Layout = ({ children }) => {
   const router = useRouter()
 
+  const [showControlPanel, setShowControlPanel] = useState(false)
+
   const toggleControlPanel = (e) => {
     e.preventDefault()
+    const trigger = document.getElementsByClassName('controlPanelTrigger')[0]
     const panel = document.getElementsByClassName('controlPanel')[0]
+
     panel.classList.toggle('-translate-y-16')
+    trigger.classList.add('animate-ping')
+    setTimeout(() => {
+      trigger.classList.remove('animate-ping')
+    }, 400)
+    setShowControlPanel(!showControlPanel)
   }
 
   return (
@@ -32,13 +42,22 @@ const Layout = ({ children }) => {
           <div className='flex justify-between z-20'>
             <Nav />
             <div className='flex'>
-              <div className='controlPanel flex items-center gap-4 -translate-y-16  transition-all duration-300'>
+              <div className='controlPanel flex items-center gap-4 -translate-y-16 transition-all duration-300' >
                 <LogoutBtn />
                 <DarkModeToggle />
-              </div>
-              <div onClick={toggleControlPanel}>
                 <LoginBtn />
               </div>
+              <button onClick={toggleControlPanel} className='mr-2 ml-4 controlPanelTrigger'>
+                {showControlPanel ?
+                  <svg xmlns="http://www.w3.org/2000/svg" className="h-12 w-12 text-brand hover:text-slate-400 hover:scale-105" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M5 11l7-7 7 7M5 19l7-7 7 7" />
+                  </svg>
+                  :
+                  <svg xmlns="http://www.w3.org/2000/svg" className="h-12 w-12 text-brand hover:text-slate-400 hover:scale-105" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M19 13l-7 7-7-7m14-8l-7 7-7-7" />
+                  </svg>
+                }
+              </button>
             </div>
           </div>
         </>
