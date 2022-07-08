05e2047f3b54935342eaed20705392ec55e26007
Christof <bauer.christof@gmail.com>
2022-07-07 20:12:45 -0500

    Even better /admin/appointments/[id]

--- pages/admin/appointments/[id].js
+++ pages/admin/appointments/[id].js
@@ -10,7 +10,7 @@ import MapLocation from '../../../components/services/MapLocation'
 import BackBtn from '../../../components/BackBtn'
 
 const Appointment = ({ appointment }) => {
-  const { id, type, date, done, time, service_option, dogs, pickups, extras } = appointment
+  const { id, type, date, done, time, service_option, dogs, users, pickups, extras } = appointment
   let image_url, coordinates, phone_number
 
   if (pickups?.length) {
@@ -50,18 +50,21 @@ const Appointment = ({ appointment }) => {
       <div className='px-4 md:px-8 py-24 mx-auto'>
         <BackBtn href='/admin/appointments' />
 
-        <div className='flex flex-col md:flex-row items-start md:items-center justify-center text-left gap-4 md:gap-12 whitespace-nowrap text-2xl md:border-b border-brand w-max mx-auto'>
+        <div className='flex flex-col md:flex-row items-start md:items-center justify-center text-left gap-4 md:gap-12 whitespace-nowrap text-2xl md:border-b border-brand max-w-max mx-auto mt-4'>
           <p><span className='text-xs block'>Date/Time </span>{date} at {time}</p>
           <p><span className='text-xs block'>Service </span><span className=' capitalize'>{type.split('-').join(' ')}</span></p>
           <p><span className='text-xs block'>Dog </span>{dogs.name}</p>
+          <p><span className='text-xs block'>User </span>{users.username}</p>
           <p><span className='text-xs block'>Delivery/Pickup </span>{service_option ? `Yes` : `No`}</p>
+          <p className='md:hidden'><span className='text-xs block'>Phone Number </span><a href={`tel:${phone_number}`}>{phone_number}</a><span className='text-xs'>(Click to call)</span></p>
+          <p className='md:hidden text-xs whitespace-normal'>Booked Extras: {extras}</p>
         </div>
-        <div className='text-xs mt-2'>
-          <p>Phone Number: <a href={`tel:${phone_number}`} className='text-xl'>{phone_number}</a> (Click to call)</p>
+
+        <div className='hidden md:block'>
+          <p className='text-xs mt-2'>Phone Number: <a href={`tel:${phone_number}`} className='text-xl'>{phone_number}</a> (Click to call)</p>
+          <p className='text-xs mt-1'>Booked Extras: <span>{extras}</span></p>
         </div>
-        <div className='text-xs mt-1'>
-          <p>Booked Extras: <span>{extras}</span></p>
-        </div>
+
         {service_option && pickups.length === 0 &&
           <div className='max-w-sm mx-auto md:max-w-max mt-4'>
             User has not yet filled out the required information for pickup/delivery service.
@@ -95,9 +98,10 @@ export async function getServerSideProps({ params }) {
   } else {
     let { data: appointment } = await supabase
       .from('appointments')
-      .select(`*, dogs(*), pickups(*)`)
+      .select(`*, dogs(*), pickups(*), users!appointments_user_fkey(*)`)
       .eq('id', id)
       .single()
+
     return {
       props: { appointment },
     }
