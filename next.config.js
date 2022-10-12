2f622ca72256f9cb2e9ee1edcb3ae908e79b2786
Christof <bauer.christof@gmail.com>
2022-10-11 22:41:20 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,12 @@
-const settings = {
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  disable: process.env.NODE_ENV === 'development',
+})
+
+module.exports = withPWA({
   i18n: {
     locales: ['en', 'es'],
     defaultLocale: 'en',
   }
-}
-
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
 })
-
-
-// module.exports = withPWA({})
-module.exports = process.env.NODE_ENV === 'development' ? settings : withPWA({ settings })
